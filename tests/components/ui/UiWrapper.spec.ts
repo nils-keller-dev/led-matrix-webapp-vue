0fb9025225cd575645d1be13bf29d213b04609b8
tsomic <keller.nils99@gmail.com>
2025-04-02 21:57:01 +0200

    ✅ add UiWrapper tests

---
+++ tests/components/ui/UiWrapper.spec.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from 'vitest'
+import UiWrapper from '../../../src/components/ui/UiWrapper.vue'
+import { shallowMount } from '@vue/test-utils'
+
+const mountingOptions = {
+  props: {
+    title: 'test title',
+    htmlFor: 'customId'
+  },
+  slots: {
+    default: 'child content'
+  }
+}
+
+describe('UiWrapper', () => {
+  test('matches snapshot', () => {
+    const wrapper = shallowMount(UiWrapper, mountingOptions)
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  test('renders title', () => {
+    const wrapper = shallowMount(UiWrapper, mountingOptions)
+
+    expect(wrapper.text()).toContain('test title')
+  })
+
+  test('renders with custom htmlFor prop', () => {
+    const wrapper = shallowMount(UiWrapper, mountingOptions)
+
+    expect(wrapper.find('label').attributes('for')).toBe('customId')
+  })
+})
